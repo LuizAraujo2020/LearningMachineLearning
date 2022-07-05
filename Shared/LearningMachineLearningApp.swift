4067306900609fdd9c08f49330129601e6ffcca0
LuizAraujo2020 <luizcarlos_bsb2006@hotmail.com>
2022-07-05 20:32:23 -0300

    Initial Commit

---
+++ Shared/LearningMachineLearningApp.swift
@@ -0,0 +1,20 @@
+//
+//  LearningMachineLearningApp.swift
+//  Shared
+//
+//  Created by Luiz Araujo on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningMachineLearningApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
